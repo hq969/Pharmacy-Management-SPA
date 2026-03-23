dc07b5b983ea2f24f2af5b5a7484321c569fc696
Harsh Sonkar <114162367+hq969@users.noreply.github.com>
2026-03-23 16:56:47 +0530

    Add ExceptionMiddleware for error handling

---
+++ backend/PharmacyApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,26 @@
+using System.Net;
+
+namespace PharmacyAPI.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(ex.Message);
+        }
+    }
+}
